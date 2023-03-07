6067fee39ca2e7a174230836fb0643fa2bf37cae
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-07 13:50:50 +0100

    WORK IN PROGRESS

---
+++ src/components/ProductTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductTable = {( products)} => {
+    return (
+        <table className="product-table">
+            
+        </table>
+    )
+}
+
+
+export default ProductTable;
