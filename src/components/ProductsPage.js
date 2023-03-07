676e6fd8767a7c101f2c8c186725209c88130b0c
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-07 14:42:45 +0100

    WORK IN PROGRESS

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import SearchBar from "./SearchBar";
-
+import ProductTable from "./ProductTable";
+import jsonData from "../data.json"
 
 
 function ProductsPage() {
-    
-    const [searchText, ] = useState("");
+    const [products, setProducts] = useState(jsonData);
+    const [searchText, setSearchText] = useState("");
+    const [inStockOnly, setInStockOnly] = useState(false);
 
 
+    const handleSearchInputChange = (e) => {
+        setSearchText(e.target.value);
+    };
 
-    
+    const handleInStockInputChange = (e) => {
+        setInStockOnly(e.target.checked);
+    };
 
 
     return (
@@ -17,8 +24,8 @@ function ProductsPage() {
             <h1>IronStore</h1>
             <SearchBar 
                 searchText={searchText}
+                onSearchInputChange={handleSearchInputChange}
 
-                
             />
             
         </div>
