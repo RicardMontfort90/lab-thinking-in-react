9b4d34b310a079467b9723988545c9549653de44
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-07 13:55:22 +0100

    WORK IN PROGRESS

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar ({ searchText, inStockOnly, onSearchInputChange, onInStockInputChange }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchBar;
