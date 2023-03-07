5d830b8e7e4045b103efc0a9e1ed27a78d5ff0ad
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-07 14:03:31 +0100

    WORK IN PROGRESS

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+import jsonData from "/data.json";
+
 
 function ProductsPage() {
+    const [products, setProducts] = useState(jsonData);
+
+
 
     return (
         <div>
-
+            <h1>IronStore</h1>
+            <SearchBar 
+                
+            />
         </div>
     )
 }
